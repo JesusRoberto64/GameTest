0ad7b00ca7fbbe54a08b030d71497dddb396385c
JesusRoberto64 <40547350+JesusRoberto64@users.noreply.github.com>
2020-03-12 14:05:55 -0600

    Safe Vertion
    Esta trabajndo con todo

---
+++ GameTest/ScenScripts/GameScene.swift
@@ -0,0 +1,70 @@
+//
+//  GameScene.swift
+//  GameTest
+//
+//  Created by Jesus Roberto Gonzalez Fonseca  on 2/28/20.
+//  Copyright © 2020 Jesus Roberto Gonzalez Fonseca . All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    var labelFooter: SKNode!
+    var btnNext: SKSpriteNode!
+    var btnPrevous: SKSpriteNode!
+    var btnSound: SKSpriteNode!
+    var btnHome: SKSpriteNode!
+    var btnStart: SKSpriteNode!
+    var nextScene: SKScene!
+    var homeScene: SKScene!
+    //DEclaracion de Ndoso
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        labelFooter = childNode(withName: "Footer")
+        btnNext = childNode(withName: "//NextButton") as! SKSpriteNode
+        btnPrevous = childNode(withName: "//PrevousButton") as! SKSpriteNode
+        btnSound = childNode(withName: "//SoundButton") as! SKSpriteNode
+        btnHome = childNode(withName: "//HomeButton") as! SKSpriteNode
+        btnStart = childNode(withName: "//StartButton") as! SKSpriteNode
+        nextScene = SKScene(fileNamed: "Scene_00") as! SKScene
+        //homeScene = SKScene(fileNamed: "GameScene") as! SKScene
+    }
+    
+    
+    override func didMove(to view: SKView) {
+ 
+    }
+    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        guard let touch = touches.first else {return}
+        
+        let touchLocation = touch.location(in: self)
+        
+        if btnStart.contains(touchLocation){
+            goToScene(scene: nextScene)
+        }
+        
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    // Fuction to trancitions
+    
+    func goToScene(scene: SKScene){
+        let sceneTransition = SKTransition.fade(with: UIColor.darkGray, duration: 1.5)
+        scene.scaleMode = .aspectFill
+        self.view?.presentScene(scene, transition: sceneTransition)
+        
+    }
+    
+    
+}
---
+++ GameTest/ScenScripts/Scene_00.swift
@@ -0,0 +1,16 @@
+//
+//  Scene_00.swift
+//  GameTest
+//
+//  Created by Jesus Roberto Gonzalez Fonseca  on 12/03/20.
+//  Copyright © 2020 Jesus Roberto Gonzalez Fonseca . All rights reserved.
+//
+
+import UIKit
+
+class Scene_00: GameScene {
+    
+    
+  
+    
+}
